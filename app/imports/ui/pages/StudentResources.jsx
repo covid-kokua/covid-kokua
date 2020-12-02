eaf1fa0c0c3982eac8b8ab7d0a6c1f09295c2c93
ltsamous <57924021+ltsamous@users.noreply.github.com>
2020-12-01 20:34:32 -1000

    M2-Updates

--- app/imports/ui/pages/StudentResources.jsx
+++ app/imports/ui/pages/StudentResources.jsx
@@ -1,60 +1,94 @@
 import React from 'react';
-import { Container, Image, Header, List } from 'semantic-ui-react';
+import { Container, Image, Grid, Card } from 'semantic-ui-react';
 
 class StudentResources extends React.Component {
   render() {
     return (
-        <div className='homepage'>
-          <Container className='home'>
-            <Header as='h1' textAlign='center'>Student Resources</Header>
-            <Image centered src="/images/CampusCenter.jpg"/>
-            <Header as='h1' textAlign='fitted left'>UH Student Relief Fund</Header>
-            <p>
-             The goal of the Student Relief Fund is to help students financially who have fallen on difficult times
-              while in school due to the coronavirus pandemic. This Fund is not a substitute for
-              financial aid and must be used only for unplanned emergencies
-              which inhibit the academic success of the student. Such emergencies can include but not limited to:
-            </p>
-            <List bulleted>
-              <List.Item>loss of employment or housing</List.Item>
-              <List.Item>family emergency</List.Item>
-              <List.Item>food insecurity</List.Item>
-              <List.Item>medical expenses</List.Item>
-              <List.Item>natural disaster or pandemic</List.Item>
-            </List>
+        <div className='student'>
+          <Container className='student'>
+            <Grid centered columns={3}>
+              <h1>Student Services</h1>
+              <h3>If you are a student looking for helpful COVID-19 information, click on the resources
+                below. You can find descriptions as well as contact information, websites, ect.</h3>
+              <Grid.Row>
+                <Grid.Column>
+                  <Card>
+                    <Image
+                        src='/images/CampusCenter.jpg'
+                        wrapped ui={false}/>
+                    <Card.Content>
+                      <Card.Header>UH Student Relief Fund</Card.Header>
+                      <Card.Description>
+                        The goal of the Student Relief Fund is to help students financially who have fallen on difficult times
+                        while in school due to the coronavirus pandemic. This Fund is not a substitute for
+                        financial aid and must be used only for unplanned emergencies
+                        which inhibit the academic success of the student.
 
-            <Header as='h1' textAlign='fitted left'> Hamilton Library Remote Resources</Header>
-            <Image src="/images/hamilton-library.jpg"/>
-            <p>
-              Hamilton Library offers remote consultation and assistance as well as other library resources online.
-              They can be reached at <a href='https://manoa.hawaii.edu/library/remote-resources/.'
-                                        target='new'>Hamilton Library</a>
-            </p>
-            <Header as='h1' textAlign='fitted left'> Learning Assistance Center</Header>
-            <p>
-              The Learning Assistance Center will be available for tutoring through online video meetings. The LAC
-              can be reached by visiting <a href='https://manoa.hawaii.edu/undergrad/Learning/tutoring/'
-                                            target='new'>Learning Assistance Center</a>. The subjects available for tutoring are:
-            </p>
-            <List bulleted>
-              <List.Item>Math</List.Item>
-              <List.Item>Science</List.Item>
-              <List.Item>Statistics</List.Item>
-              <List.Item>Business</List.Item>
-              <List.Item>Language</List.Item>
-            </List>
-            <Header as='h1' textAlign='fitted left'> Counseling and Student Development Center</Header>
-            <p>
-              The CSDC offers a variety of services to students and can be very helpful during this period of stress and anxiety.
-              They can be reached by visiting <a href='https://manoa.hawaii.edu/counseling/'
-                                                 target='new'>Counseling and Student Development Center</a>. Some of their Services include
-            </p>
-            <List bulleted>
-              <List.Item>Hawaii CARES Hotline</List.Item>
-              <List.Item>National Suicide Prevention Lifeline</List.Item>
-              <List.Item>The Disaster Destress Hotline</List.Item>
-              <List.Item>Worklife Hawaii</List.Item>
-            </List>
+                      </Card.Description>
+                      <Card.Description>
+                        <a href="https://www.hawaii.edu/offices/student-affairs/urgent-student-relief-fund/">Student Relief Fund</a>
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+                </Grid.Column>
+                <Grid.Column centered>
+                  <Card>
+                    <Image
+                        src='/images/hamilton-library.jpg'
+                        wrapped ui={false}/>
+                    <Card.Content>
+                      <Card.Header>Hamilton Library</Card.Header>
+                      <Card.Description>
+                        Hamilton Library offers remote consultation and assistance as well as other library resources online.
+                      </Card.Description>
+                      <Card.Description>
+                        <a href="https://manoa.hawaii.edu/library/remote-resources/">Hamilton Library</a>
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+                </Grid.Column>
+                <Grid.Column>
+                  <Card>
+                    <Image
+                        src='/images/Learning-Assistance-Center.png'
+                        wrapped ui={false}/>
+                    <Card.Content>
+                      <Card.Header>Learning Assistance Center</Card.Header>
+                      <Card.Description>
+                        The Learning Assistance Center will be available for tutoring through online video meetings.
+                      </Card.Description>
+                      <Card.Description>
+                        <a href="https://manoa.hawaii.edu/undergrad/Learning/tutoring/">Learning Assistance Center</a>
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Card>
+                    <Image
+                        src='/images/CSDC.jpg'
+                        wrapped ui={false}/>
+                    <Card.Content>
+                      <Card.Header>Counseling and Student Development Center</Card.Header>
+                      <Card.Description>
+                        The CSDC offers a variety of services to students and can be very helpful
+                        during this period of stress and anxiety.
+                      </Card.Description>
+                      <Card.Description>
+                        <a href="https://manoa.hawaii.edu/counseling/">Counseling and Student Development Center
+                            </a>
+                      </Card.Description>
+                    </Card.Content>
+                  </Card>
+                </Grid.Column>
+                <Grid.Column>
+                </Grid.Column>
+                <Grid.Column>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
           </Container>
         </div>
     );
