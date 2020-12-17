37130641d4b31345857946547dab41880b9d2b0f
Austin Chong <austin.chong4@gmail.com>
2020-12-16 20:45:48 -1000

    Made changes to Landing and NavBar color

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -7,17 +7,37 @@ class Landing extends React.Component {
     return (
         <div className='landing'>
           <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-            <Image className='landing' src="/images/oahu.jpg"/>
+            <Image className='landing' centered size='huge' src="/images/oahu.jpg"/>
             <Grid.Column width={8}>
-              <h1>Welcome to #Kokua-Hawai&apos;i!</h1>
-              <p className='intro'>A center for all COVID related resources such as financial assistance, social services, and other
+              <h1 className='introHeader'>Welcome to #Kokua-Hawai&apos;i!</h1>
+              <p className='intro'>A center for all COVID related resources such as financial assistance, social
+                services, and other
                 services that will help our islands.</p>
             </Grid.Column>
           </Grid>
-          <iframe src="https://public.domo.com/cards/aAR5B" width="100%" height="600" marginHeight="0" marginWidth="0" frameBorder="0"></iframe>
+          <Container>
+            <iframe src="https://public.domo.com/cards/aAR5B" width="100%" height="600" marginHeight="0" marginWidth="0"
+                    frameBorder="0"></iframe>
+            <p className='table'>
+              Explanation of the COVID-19 Table:
 
-          <Container className='home'>
+              This table is a representation of all COVID-19 cases in the United States, which is broken down into cases
+              by state. This is a real-time representation of all confirmed, new, current, and cases which resulted in
+              deaths.
+            </p>
+          </Container>
+
+          <Container textAlign='center' className='home'>
+            <Header as='h1'>How Do I Navigate #Kokua-Hawai&apos;i?</Header>
+            <p className='home'>
+              Using the navigation bar on the top of the webpage, we have made it easier to look for specific services
+              for each families needs. We understand that all families do not have the same struggles during this hard
+              time, so we have separated each service in its own section. Feel free to browse each section and find the
+              information you need in a timely, efficient manner!
+            </p>
+
             <Header as='h1' textAlign='center' className='mission'>Mission Statement</Header>
+            <Image centered size='huge' src="/images/unemployment.png"/>
             <p>
               With so many websites failing to give the proper, relevant information to our residents, #COVID-Kokua is
               designed to ensure that all COVID related resources are in one spot. We have gathered information from
@@ -29,16 +49,6 @@ class Landing extends React.Component {
               benefits,
               and other topics. With the impact of COVID-19, it is our mission to provide easy access to all the
               resources funded by the state/federal government to help those in need.
-            </p>
-            <Image centered src="/images/unemployment.png"/>
-
-            <Image centered src="/images/question.png"/>
-            <Header as='h1' textAlign='center'>How Do I Navigate #COVID-Kokua?</Header>
-            <p>
-              Using the navigation bar on the top of the webpage, we have made it easier to look for specific services
-              for each families needs. We understand that all families do not have the same struggles during this hard
-              time, so we have separated each service in its own section. Feel free to browse each section and find the
-              information you need in a timely, efficient manner!
             </p>
           </Container>
         </div>
